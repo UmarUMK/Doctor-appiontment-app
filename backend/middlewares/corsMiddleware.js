49e67b5e48cdcab6f42ae6c289a02d833df9a204
UmarUMK <152306803+UmarUMK@users.noreply.github.com>
2024-10-17 19:56:30 +0530

    Update corsMiddleware.js

--- backend/middlewares/corsMiddleware.js
+++ backend/middlewares/corsMiddleware.js
@@ -1,4 +1,4 @@
-function corsMiddleware(req, res, next) {
+export const corsMiddleware=(req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin', 'your-frontend-url'); // Replace with your frontend URL
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Add methods as needed
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Add any other headers you need
